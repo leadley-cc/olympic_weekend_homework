c520be4d4beb8e21a4bb0d9c1b344126cf4045f2
Michael Leadley <leadley75@gmail.com>
2017-09-15 13:27:54 +0100

    Add SkillType

---
+++ app/src/main/java/com/example/michael/olympicsweekendhomework/SkillType.java
@@ -0,0 +1,13 @@
+package com.example.michael.olympicsweekendhomework;
+
+/**
+ * Created by michael on 15/09/2017.
+ */
+
+public enum SkillType {
+    ACCURACY,
+    AGILITY,
+    SPEED,
+    STAMINA,
+    STRENGTH
+}
