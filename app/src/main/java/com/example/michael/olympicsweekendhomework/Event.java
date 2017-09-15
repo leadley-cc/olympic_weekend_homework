e0f28273ddd76313d90a516e30ff0820ed525ede
Michael Leadley <leadley75@gmail.com>
2017-09-15 13:30:10 +0100

    Add basic Event class

---
+++ app/src/main/java/com/example/michael/olympicsweekendhomework/Event.java
@@ -0,0 +1,34 @@
+package com.example.michael.olympicsweekendhomework;
+
+import com.example.michael.olympicsweekendhomework.competitors.Competitor;
+import com.example.michael.olympicsweekendhomework.scorers.Scorer;
+
+import java.util.*;
+
+/**
+ * Created by michael on 15/09/2017.
+ */
+
+public class Event {
+    private String name;
+    private List<Competitor> competitors;
+    private Scorer scorer;
+
+    public Event(String name, List<Competitor> competitors, Scorer scorer) {
+        this.name = name;
+        this.competitors = competitors;
+        this.scorer = scorer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Competitor> getCompetitors() {
+        return competitors;
+    }
+
+    public Competitor calculateWinner() {
+        return null;
+    }
+}
