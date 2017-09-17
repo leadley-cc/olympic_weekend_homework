94bdff43e02e0182adcb8a5988153819ad847147
Michael Leadley <leadley75@gmail.com>
2017-09-17 11:55:32 +0100

    Complete awardMedals() in Event

--- app/src/main/java/com/example/michael/olympicsweekendhomework/Event.java
+++ app/src/main/java/com/example/michael/olympicsweekendhomework/Event.java
@@ -5,6 +5,8 @@ import com.example.michael.olympicsweekendhomework.scorers.Scorer;
 
 import java.util.*;
 
+import static com.example.michael.olympicsweekendhomework.MedalType.*;
+
 /**
  * Created by michael on 15/09/2017.
  */
@@ -28,9 +30,14 @@ public class Event {
         return competitors;
     }
 
-    public Competitor awardMedals() {
-        Competitor winner = null;
-
-        return null;
+    public void awardMedals() {
+        TreeSet<Score> scores = new TreeSet<>();
+        for (Competitor competitor: competitors) {
+            scores.add( scorer.calculateScore(competitor) );
+        }
+        // pollLast gets and removes the highest Score since a TreeSet is in ascending order
+        scores.pollLast().getCompetitor().awardMedal( new Medal(GOLD) );
+        scores.pollLast().getCompetitor().awardMedal( new Medal(SILVER) );
+        scores.pollLast().getCompetitor().awardMedal( new Medal(BRONZE) );
     }
 }
